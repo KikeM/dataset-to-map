08196ce667bd5b4901c9037d69daa97fa1ceb821
Enrique Millan Valbuena <enrique.millanvalbuena@gmail.com>
2024-04-02 16:46:10 +0200

    feat: generic code

--- dataset-wsfj.py
+++ dataset-wsfj.py
@@ -1,10 +1,8 @@
-from hmac import new
 from pathlib import Path
 
 import pandas as pd
 from rich import print as rprint
 from thefuzz.process import extract
-import typer
 
 choices = [
     "businessValue",
@@ -29,19 +27,21 @@ def process_string(string: str):
     return string
 
 
-def rename_columns(dataset: pd.DataFrame):
+def rename_columns(dataset: pd.DataFrame, threshold: int):
     mapping = {}
     for column in dataset.columns:
-        new_column = extract(column, choices, limit=1)
+        _input = process_string(column)
+        new_column = extract(_input, choices, limit=1)
         value, score = new_column[0]
-        if score < 80:
+        if score < threshold:
             rprint(
                 f"For column '{column}' current value is '{value}' with score ({score}), keeping it as is."
             )
             value = column
         mapping[column] = value
 
-    rprint("Mapping: ", mapping)
+    rprint("Mapping:")
+    rprint(mapping)
 
     dataset = dataset.rename(columns=mapping)
 
@@ -55,22 +55,37 @@ def load_dataset(path):
     rprint("\n[bold]Dataset Raw[/bold]\n")
     rprint(dataset)
 
-    dataset = rename_columns(dataset)
+    return dataset
 
-    dataset = dataset.dropna(subset=["wsfj"])
+
+def drop_rows_cols(dataset: pd.DataFrame):
+
+    columns = dataset.columns
+    assert "wsfj" in columns, "Column 'wsfj' is required."
+    assert "name" in columns, "Column 'name' is required."
+
+    dataset = dataset.dropna(subset=["wsfj", "name"])
     dataset = dataset.drop(
-        columns=["observations", "notes", "description"],
+        columns=[
+            "observations",
+            "notes",
+            "description",
+        ],
         errors="ignore",
     )
-    rprint("\n[bold]Dataset Clean[/bold]\n")
-    rprint(dataset)
-
     return dataset
+
+
+def enforce_csv_extension(path: Path):
+    if path.suffix != ".csv":
+        path = path.with_suffix(".csv")
+    return path
 
 
 def main(
     path: Path,
-    output: Path = "output.png",
+    threshold: int = 70,
+    output: Path = "dataset.csv",
 ):
 
     # Create dataset
@@ -78,4 +93,15 @@ def main(
 
     rprint(f"Reading from: {path}")
 
-    load_dataset(path=path)
+    dataset = load_dataset(path=path)
+    dataset = rename_columns(dataset, threshold=threshold)
+    dataset = drop_rows_cols(dataset)
+
+    dataset = dataset.sort_values(by="wsfj", ascending=False)
+
+    rprint("\n[bold]Dataset Clean[/bold]\n")
+    rprint(dataset)
+
+    output = enforce_csv_extension(output)
+    rprint(f"Saving to: {output}")
+    dataset.to_csv(output, index=False)
